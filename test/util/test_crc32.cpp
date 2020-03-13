4acb796e4ec83ed19def3a914a029483ef51383e
npangunion <npangunion@gmail.com>
2020-03-13 10:55:33 +0900

    - 테스트 파일 추가.

--- test/util/test_crc32.cpp
+++ test/util/test_crc32.cpp
@@ -1,1 +1,17 @@
 #include <pch.hpp>
+#include <catch.hpp>
+#include <wise.kernel/core/logger.hpp>
+#include <wise.kernel/util/crc32.hpp>
+
+TEST_CASE("crc32", "util")
+{
+	SECTION("usage")
+	{
+		std::string data("Hello Crc32");
+		
+		uint32_t crc1 = wise::kernel::crc32(data.begin(), data.end());
+
+		WISE_INFO("crc:{}", crc1);
+	}
+
+}
