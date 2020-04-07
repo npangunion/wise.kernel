f040a5a0938322469a2a8cb42ed6f3a35dcf0545
npangunion <npangunion@gmail.com>
2020-04-07 14:39:21 +0900

    액터

--- src/server/actor.cpp
+++ src/server/actor.cpp
@@ -1,1 +1,43 @@
 #include <pch.hpp>
+#include <wise.kernel/server/actor.hpp>
+#include <wise.kernel/core/fmt.hpp>
+
+namespace wise
+{
+namespace kernel 
+{
+
+actor::actor(server& _server, id_t parent, id_t id)
+	: ch_(fmt::format("actor_{}", id))
+	, server_(_server)
+{
+}
+
+actor::actor(server& _server, id_t id)
+	: ch_(fmt::format("actor_{}", id))
+	, server_(_server)
+{
+}
+
+actor::~actor()
+{
+}
+
+bool actor::on_start()
+{
+	return init();
+}
+
+task::result actor::on_execute()
+{
+	ch_.execute();
+	timer_.execute();
+}
+
+void actor::on_finish()
+{
+	fini();
+}
+
+} // kernel
+} // wise
